9ab3763724f44c1359c588c39770f59e1f1ade5f
Elanrif <saidbacoelanrif@gmail.com>
2023-10-23 23:05:20 +0100

    second commit

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -18,15 +18,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main2);/* on démarre par défaut le layout.activity_main2*/
 
         this.play = (ImageView) findViewById(R.id.play);
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*on va démarrer une nouvelle activité avec Intent*/
                 Intent otherActivity = new Intent(getApplicationContext(),CookieActivity.class);
-                startActivity(otherActivity);
-                finish();
+                startActivity(otherActivity);/* on lance l'activité*/
+                finish();/* on doit arreté celui-ci en cours.*/
             }
         });
     }
